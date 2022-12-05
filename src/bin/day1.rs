45e7eddb7ef18927109b2c97e497b6a51127a647
Thomas Frans <franske2000@gmail.com>
2022-12-05 19:20:51 +0100

    day1

---
+++ src/bin/day1.rs
@@ -0,0 +1,57 @@
+use std::{fs::File, io::{BufReader, BufRead}, process::exit};
+
+use itertools::Itertools;
+
+
+fn main() -> Result<(), String> {
+    let file_location = "data/day1.txt";
+
+    let Ok(file) = File::open(file_location) else {
+        return Err(format!("Failed to open file {}, maybe it doesn't exist.", file_location));
+    };
+
+    let file_reader = BufReader::new(file);
+
+    let groups = file_reader.lines()
+        .map(|line| {
+            let Ok(line) = line else {
+                eprintln!("Line {line:?} isn't a valid string.");
+                exit(1);
+            };
+            line
+        })
+        .group_by(|line| {
+            line.is_empty()
+        });
+
+
+    let elves = groups.into_iter()
+        .enumerate()
+        .filter(|(index, _)| {
+            if *index % 2 == 0 {
+                true
+            } else {
+                false
+            }
+        })
+        .map(|(_, other)| {
+            other
+        });
+
+    let calories = elves.map(|(_, group)| {
+        let mut total = 0;
+        for item in group {
+            total += item.parse::<u32>().unwrap();
+        }
+        total
+    })
+    .sorted()
+    .rev()
+    .collect::<Vec<_>>();
+
+    let total = calories[0] + calories[1] + calories[2];
+
+    println!("total top 3 cals: {}", total);
+
+    Ok(())
+}
