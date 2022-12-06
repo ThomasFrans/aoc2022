396bc91575a767a9b61485f76218abf3d1ea6c2b
Thomas Frans <franske2000@gmail.com>
2022-12-06 13:44:18 +0100

    Clean up.

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -6,10 +6,12 @@ use std::{
 use crate::message::ElfMessage;
 
 struct ProgramArguments {
+    /// The filename of the file that contains the message to decode.
     filename: String,
 }
 
 impl ProgramArguments {
+    /// Get the arguments automatically.
     fn from_env() -> Result<ProgramArguments, String> {
         let args = env::args();
         // Skip the program name.
@@ -32,6 +34,11 @@ mod message {
     }
 
     impl ElfMessage {
+        /// The header as specified in the hyper elf transfer protocol v6.
+        ///
+        /// Returns all the data until the header has been read, which is all
+        /// the data up to and including the first occurrence of 4 unique
+        /// characters.
         pub fn header(&self) -> Result<String, &'static str> {
             let data = self.data.chars().collect::<Vec<_>>();
             for chunk in 0..data.len() - HEADER_SIZE {
@@ -43,10 +50,16 @@ mod message {
             Err("Couldn't find header.")
         }
 
+        /// The start of message header as specified in the hyper elf transfer
+        /// protocol v6.
+        ///
+        /// Returns all the data until the header has been read, which is all
+        /// the data up to and including the first occurrence of 14 unique
+        /// characters.
         pub fn start_of_message_header(&self) -> Result<String, &'static str> {
-            // Copying is ok because iterating over &char would probably require the
-            // same amount of information to be created per char, since it has to
-            // know the length of a char.
+            // Copying is ok because iterating over &char would probably require
+            // the same or more information to be created per char, since it has
+            // to know the length of a char.
             let data = self.data.chars().collect::<Vec<_>>();
             for chunk in 0..data.len() - START_OF_MESSAGE_HEADER_SIZE {
                 let chunked = &data[chunk..chunk + START_OF_MESSAGE_HEADER_SIZE];
@@ -61,17 +74,19 @@ mod message {
     }
 }
 
-struct Error {
+/// A type that allows easy branching from `main()` while also displaying clean
+/// error messages to the user.
+struct ProgramError {
     error: Box<dyn Display>,
 }
 
-impl Debug for Error {
+impl Debug for ProgramError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.error)
     }
 }
 
-impl<T: Display + 'static> From<T> for Error {
+impl<T: Display + 'static> From<T> for ProgramError {
     fn from(error: T) -> Self {
         Self {
             error: Box::new(error),
@@ -79,11 +94,9 @@ impl<T: Display + 'static> From<T> for Error {
     }
 }
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<(), ProgramError> {
     let arguments = ProgramArguments::from_env()?;
-
     let input = std::fs::read_to_string(&arguments.filename)?;
-
     let message = ElfMessage { data: input };
 
     // Part 1
@@ -94,5 +107,6 @@ fn main() -> Result<(), Error> {
         "Length of start of message header is {}.",
         message.start_of_message_header()?.len()
     );
+
     Ok(())
 }
